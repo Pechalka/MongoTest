702c9d767c1432990d05ede8a24af8b560bc402c
Andrew <luzkovskyandrey@gmail.com>
2013-01-17 23:36:09 +0300

    add user form base

---
+++ site/view_models/add_user.js
@@ -0,0 +1,26 @@
+define(["knockout", "jquery", "ko.mapping"],function(ko, $, mapping) {	
+	return function (model){
+		// var self = this;
+		// self.save = function(){	
+		// 	if (!self.is_valide())
+		// 		return;
+
+		// 	var data = ko.toJS(self.dishe);
+		// 	data.category = self.dishe.category();
+			
+		// 	$.post('/api/dishes', data, model.on_save);
+		// }
+
+		// self.is_valide = function(){
+		// 	return self.dishe.name().length > 0;
+		// }
+
+		// self.dishe = mapping.fromJS( model.dishe );
+		// self.categories = model.categories;
+
+		self.save = function(){
+
+			model.on_save();
+		};
+	}
+});
--- site/view_models/users.js
+++ site/view_models/users.js
@@ -1,14 +1,29 @@
-define(["knockout", "jquery", "Grid", "ko.mapping"],
-	function(ko, $, Grid, ko_mapping) {
+define(["knockout", "jquery", "Grid", "ko.mapping", "render"],
+	function(ko, $, Grid, ko_mapping, render) {
 		return function(model){
 			var self = this;
             
+			self.popup = ko.observable(null);
+
             self.filterParams = ko_mapping.fromJS(model);
             self.grid = new Grid('/api/users', {"Field":"FirstName","Distinct":"asc"}, self);
 
 
             self.filted = function() {
                 self.grid.refresh({ pageNumber : 1, filterParams : ko.toJS(self.filterParams) });
+            };
+
+
+            self.add = function(){
+				render(self.popup, "add_user", {
+					//categories : self.categories,
+					//dishe : { category : self.chosen_category(), price : 0, name : '', description : '' },
+					on_save : function(){
+					//	self.fetch_dishes();
+						$('#popup').modal('hide');
+					}
+				});
+				$('#popup').modal('show');
             };
 		}
 	});
