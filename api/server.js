33fff39cc6fc151eaf8a1ee385ed78b5b48fc730
Tonko Andrew <tonko.andrew@gmail.com>
2013-01-18 01:30:46 +0300

    Pohui

---
+++ api/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.use(express.static(__dirname + "/../site/"));
+app.use(express.bodyParser());
+
+
+
+app.post('/api/users', function(req, res){
+    console.log(req.body);
+
+    res.json(
+        {
+         Data : [
+          { FirstName : 'Vasa', LastName : 'Ivanov', Gender : true, Age : 15},
+          { FirstName : 'Vasa9', LastName : 'Ivanov', Gender : true, Age : 15}
+         ],
+         Paging : {
+          PageNumber : req.body.pageNumber, 
+          TotalPagesCount : 10
+         }
+        }
+    );
+});
+
+app.listen(3000);
